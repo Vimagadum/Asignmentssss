3f2af21bbae6b7fa79785befbe0abd6adbd2ad69
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 16:05:49 +0530

    [Vinayak] adding core programs

---
+++ LargestNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programss
+{
+    internal class LargestNumber
+    {
+        public static void Number()
+        {
+            Console.WriteLine("Enter a first num: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second num: ");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a third num: ");
+            int third = int.Parse(Console.ReadLine());
+
+
+            if (first > second && first > third)
+            {
+                Console.WriteLine(first + " is  greater than " + second + " and " + third);
+            }
+            else if (second > third)
+            {
+                Console.WriteLine(second + " is  greater than " + first + " and " + third);
+            }
+            else
+            {
+                Console.WriteLine(third + " is  greater than " + first + " and" + second);
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Programss;
+using assignment;
+
+namespace Programss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Fibonaccy.findFibonacciSeries();
+            //Harmonic.PrintHarmonicNumber();
+            //FlipCoin.flipCoinPercentage();
+            //ReverseNumber.findReverse();
+
+            //ReplaceString Replacee = new ReplaceString();
+            //Replacee.ReadingInputFromUser();
+            //PowerOfTwo.GetPowerOfTwo();
+
+            //Factorial.findFact();
+
+            //SwapNumber.readInputFromUser();
+
+            //Even_Odd.number();
+            //Leap_Year.year();
+            //Quotient_Reminder.number();
+            //VowelorConsonant.charecter();
+            //LargestNumber.Number();
+
+
+
+        }
+    }
+}
